6f603eb01233135bb2b166c7946ba6492e65e882
andrew-matt <andrew.matt@ya.ru>
2022-04-09 11:31:29 +0300

    update main

--- src/NativeJSLessons/Lesson 6/main.js
+++ src/NativeJSLessons/Lesson 6/main.js
@@ -44,52 +44,61 @@ const user = {
 }
 
 //1. Создайте поверхностную копию объекта user
-let copyUser;
+let copyUser = {
+    ...user
+}
 
 //Проверка:
-// console.log(user===copyUser)- что должно быть в консоли?
-// console.log(user.friends===copyUser.friends)- что должно быть в консоли?
+console.log(user===copyUser) // false
+console.log(user.friends===copyUser.friends) // true
 
 //2. Полная (глубокая) копия объекта user
-let deepCopyUser;
+let deepCopyUser = {
+    ...user,
+    friends: [
+        ...user.friends
+    ]
+}
 
 //Проверка:
-// console.log(user===deepCopyUser) - что должно быть в консоли?
-// console.log(user.friends===deepCopyUser.friends) - что должно быть в консоли?
+console.log(user===deepCopyUser) // false
+console.log(user.friends===deepCopyUser.friends) // false
 
 //3. Поверхностная копия массива students
-let copyStudents;
+let copyStudents = [
+    ...students
+]
 
 //Проверка:
-// console.log(код проверки написать самостоятельно ) - что должно быть в консоли?
-// console.log(код проверки написать самостоятельно) - что должно быть в консоли?
+console.log(students===copyStudents) // false
+console.log(students[0]===copyStudents[0]) // true
 
 //4*. Полная (глубокая) копия массива students (map)
-let deepCopyStudents;
+let deepCopyStudents = students.map(s => ({...s}))
 
 //Проверка:
-// console.log(код проверки написать самостоятельно) - что должно быть в консоли?
-// console.log(код проверки написать самостоятельно) - что должно быть в консоли?
+console.log(students===deepCopyStudents) // false
+console.log(students[0]===deepCopyStudents[0]) // false
 
 // NB!!! Далее все преобразования выполняем не модифицируя исходный массив students
 // Вывод результатов - в консоль
 
 //5. Отсортируйте копию массива deepCopyStudents по алфавиту (sort)
-let sortedByName;
+let sortedByName = deepCopyStudents.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0)
 console.log(sortedByName);
 
 //5a. Отсортируйте deepCopyStudents по успеваемости(лучший идёт первым)(sort)
-let sortedByScores;
+let sortedByScores = deepCopyStudents.sort((a, b) => b.scores - a.scores)
 console.log(sortedByScores);
 
 //6. Сформируйте массив студентов, у которых 100 и более баллов (filter)
-let  bestStudents;
+let  bestStudents = students.filter(s => s.scores >= 100)
 console.log(bestStudents)
 
 //6a. Получите массив ("вырежьте") из трёх лучших студентов из массива deepCopyStudents (splice)
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
 
-let topStudents;
+let topStudents = deepCopyStudents.sort((a, b) => b.scores - a.scores).splice(0, 3)
 console.log(topStudents)
 console.log(deepCopyStudents)
 
