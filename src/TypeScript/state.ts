35d4f52e44f01b7b3957a69fb0d04a78920dac5f
andrew-matt <andrew.matt@ya.ru>
2022-04-15 10:32:11 +0300

    update state file

--- src/TypeScript/state.ts
+++ src/TypeScript/state.ts
@@ -18,4 +18,29 @@ let car: CarType = {
     }
 }
 
+interface IGarage {
+    addCar: (car: CarType) => void
+    logAllCarsNames: () => void
+    getAllCars: () => CarType[]
+}
+
+let createGarage = (): IGarage => {
+    let _cars: CarType[] = [];
+
+    return {
+        addCar(car) {
+            _cars.push(car);
+        },
+        logAllCarsNames() {
+            console.log('Cars in the garage: ');
+            _cars.forEach(c => console.log(c.model));
+        },
+        getAllCars() {
+            return _cars;
+        }
+    }
+}
+
+
+
 export default 1;
