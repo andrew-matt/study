c53a77c1544fa7ea77d2443281c1419d4e9156ba
andrew-matt <andrew.matt@ya.ru>
2022-05-05 12:20:03 +0300

    add post-request

--- src/AJAX/index.js
+++ src/AJAX/index.js
@@ -1,16 +1,35 @@
 console.log(0)
 
 const resultBlock = document.querySelector("#result");
-const clickMeButton = document.querySelector("#click-me");
 const pageNumberEl = document.querySelector("#page-number");
+const clickMeButton = document.querySelector("#click-me");
+const getTasksButton = document.querySelector("#get-tasks");
 
 clickMeButton.addEventListener("click", () => {
     const promise = getImages(pageNumberEl.value);
-    promise.then(onDataReceived)
+    promise.then(onImagesReceived)
 });
 
-function onDataReceived(data) {
-    data.forEach(el => {
+getTasksButton.addEventListener("click", () => {
+    const promise = getTasks();
+    promise.then(onTasksReceived)
+});
+
+createTasks('learn JS').then(data => {
+    debugger
+    console.log(data)
+})
+
+function onTasksReceived(tasks) {
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        li.innerHTML = task;
+        document.querySelector('#tasks-result').appendChild(li);
+    });
+}
+
+function onImagesReceived(array) {
+    array.forEach(el => {
         const img = document.createElement('img');
         img.src = el.thumbnail;
         document.querySelector('#result').appendChild(img);
--- src/AJAX/service.js
+++ src/AJAX/service.js
@@ -1,4 +1,15 @@
 function getImages(pageNumberEl) {
     const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumberEl}&count=1`)
-    return promise.then(data => data.data)
+    return promise.then(response => response.data)
 }
+
+function getTasks() {
+    const promise = axios.get(`https://repetitora.net/api/JS/Tasks?widgetId=123`)
+    return promise.then(response => response.data)
+}
+
+function createTasks(title) {
+    const promise = axios.post(`https://repetitora.net/api/JS/Tasks?widgetId=123&title=${title}`)
+    return promise.then(response => response.data)
+}
+
