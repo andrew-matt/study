69fa964e4e24d8b94146c6035c24352b2c13ebd1
andrew-matt <andrew.matt@ya.ru>
2022-05-05 10:30:46 +0300

    add promises and axios

--- src/AJAX/index.js
+++ src/AJAX/index.js
@@ -2,17 +2,17 @@ console.log(0)
 
 const resultBlock = document.querySelector("#result");
 const clickMeButton = document.querySelector("#click-me");
-const pageNumber = document.querySelector("#page-number");
+const pageNumberEl = document.querySelector("#page-number");
+
+clickMeButton.addEventListener("click", () => {
+    const promise = getImages(pageNumberEl.value);
+    promise.then(onDataReceived)
+});
 
 function onDataReceived(data) {
     data.forEach(el => {
-        const img = document.createElement('img')
-        img.src = el.thumbnail
-        document.querySelector('#result').appendChild(img)
-    })
+        const img = document.createElement('img');
+        img.src = el.thumbnail;
+        document.querySelector('#result').appendChild(img);
+    });
 }
-
-clickMeButton.addEventListener("click", () => {
-    getImages(pageNumber.value, onDataReceived);
-});
-
--- src/AJAX/service.js
+++ src/AJAX/service.js
@@ -1,7 +1,4 @@
-function getImages(pageNumberEl, successCallback) {
-    $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumberEl}&count=1`, {
-        success: function (data) {
-            successCallback(data)
-        }
-    })
+function getImages(pageNumberEl) {
+    const promise = axios.get(`https://repetitora.net/api/JS/Images?page=${pageNumberEl}&count=1`)
+    return promise.then(data => data.data)
 }
