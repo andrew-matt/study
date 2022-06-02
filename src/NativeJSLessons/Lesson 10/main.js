1ec6b49d2edddb910df3944cb8c67e24177caba0
andrew-matt <andrew.matt@ya.ru>
2022-06-02 12:32:23 +0300

    add resolve example

--- src/NativeJSLessons/Lesson 10/main.js
+++ src/NativeJSLessons/Lesson 10/main.js
@@ -1,19 +1,23 @@
 let p = new Promise((resolve, reject) => {
     setTimeout(() => {
         const user = {name: "Alex"}
-    }, 1000)
 
-    reject({status: 400, message: 'Bad request'})
+        resolve(user)
+
+        reject({status: 400, message: 'Bad request'})
+
+    }, 1000)
 })
 
-p.then(
-    (res) => {
-        console.log('Inside then first arg')
-        console.log(res)
-    },
-    (err) => {
-        console.log('Inside then second arg')
-        console.log(err)
-    },
-)
+p
+    .then(
+        (res) => {
+            console.log('Inside then first arg')
+            console.log(res)
+        })
+    .catch(
+        (err) => {
+            console.log('Inside then second arg')
+            console.log(err)
+        })
 
