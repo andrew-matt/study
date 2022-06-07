01a26b3a81143033b18adbb945608746cd1693c1
andrew-matt <andrew.matt@ya.ru>
2022-06-07 18:31:15 +0300

    update main.js

--- src/NativeJSLessons/Lesson 10/main.js
+++ src/NativeJSLessons/Lesson 10/main.js
@@ -1,30 +1,43 @@
-let p = new Promise((resolve, reject) => {
-    setTimeout(() => {
+// let p = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//
+//         resolve(1)
+//
+//     }, 1000)
+// })
+//
+// p
+//     .then(
+//         (res) => {
+//             console.log(res)
+//             return res
+//         })
+//     .then(
+//         (res) => {
+//             console.log(res)
+//         })
+//
+// let all = Promise.all([
+//     new Promise(res => setTimeout(() => res(1), 3000)),
+//     new Promise(res => setTimeout(() => res(2), 2000)),
+//     new Promise(res => setTimeout(() => res(3), 1000)),
+// ])
+//
+// all.then(res => {
+//     console.log(res)
+// })
+//
+//
 
-        resolve(1)
+setTimeout(function() {
+    console.log(1)
+}, 0)
 
-    }, 1000)
+new Promise((resolve, reject) => {
+    console.log(2)
+    resolve()
+}).then(() => {
+    console.log(3)
 })
 
-p
-    .then(
-        (res) => {
-            console.log(res)
-            return res
-        })
-    .then(
-        (res) => {
-            console.log(res)
-        })
-
-let all = Promise.all([
-    new Promise(res => setTimeout(() => res(1), 3000)),
-    new Promise(res => setTimeout(() => res(2), 2000)),
-    new Promise(res => setTimeout(() => res(3), 1000)),
-])
-
-all.then(res => {
-    console.log(res)
-})
-
-
+console.log(4)
