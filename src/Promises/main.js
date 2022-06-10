690ffd05d9ee14ae826d787b879024a31e018fd0
andrew-matt <andrew.matt@ya.ru>
2022-06-10 15:13:26 +0300

    update main.js

--- src/Promises/main.js
+++ src/Promises/main.js
@@ -1,17 +1,17 @@
-// function sayHi(name, surname) {
-//     console.log(`${name} ${surname}`)
-// }
-//
-// sayHi("Alex", "Viarhey")
+/*function sayHi(name, surname) {
+    console.log(`${name} ${surname}`)
+}
 
- function counterCreator() {
+sayHi("Alex", "Viarhey")*/
 
-    let count = 0;
+/*function counterCreator() {
 
-    return function () {
-        return count++
-    }
- }
+   let count = 0;
+
+   return function () {
+       return count++
+   }
+}
 
 let counter = counterCreator();
 let counter2 = counterCreator();
@@ -26,4 +26,24 @@ counter2();
 counter2();
 
 console.log(counter())
-console.log(counter2())
+console.log(counter2())*/
+
+function wait(ms) {
+    return new Promise((res) => {
+        setTimeout(() => {
+            return res()
+        }, ms)
+    })
+}
+
+async function run() {
+    await wait(1000)
+    console.log(1)
+    await wait(1000)
+    console.log(2)
+    await wait(1000)
+    console.log(3)
+}
+
+run()
+
